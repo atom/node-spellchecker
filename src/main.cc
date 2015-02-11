f5ab5a42f91fb0832e035e76c843213066511dbe
Paul Betts <paul@paulbetts.org>
2015-02-10 17:38:17 -0800

    Create a Node class that wraps Hunspell for the moment

--- src/main.cc
+++ src/main.cc
@@ -1,49 +1,97 @@
-#include "spellchecker.h"
-
 #include "nan.h"
+#include "spellchecker.h"
+#include "spellchecker_hunspell.h"
+
+using node::ObjectWrap;
+using namespace spellchecker;
 using namespace v8;
 
 namespace {
 
-NAN_METHOD(PlatformInit) {
-  NanScope();
-  spellchecker::Init(*String::Utf8Value(args[0]));
-  NanReturnUndefined();
-}
+class Spellchecker : public ObjectWrap {
+  SpellcheckerImplementation* impl;
 
-NAN_METHOD(IsMisspelled) {
-  NanScope();
-  if (args.Length() < 1)
-    return NanThrowError("Bad argument");
+  static NAN_METHOD(New) {
+    NanScope();
+    Spellchecker* that = new Spellchecker();
+    that->Wrap(args.This());
 
-  std::string word = *String::Utf8Value(args[0]);
-  NanReturnValue(NanNew<Boolean>(spellchecker::IsMisspelled(word)));
-}
-
-NAN_METHOD(GetCorrectionsForMisspelling) {
-  NanScope();
-  if (args.Length() < 1)
-    return NanThrowError("Bad argument");
-
-  std::string word = *String::Utf8Value(args[0]);
-  std::vector<std::string> corrections =
-    spellchecker::GetCorrectionsForMisspelling(word);
-
-  Local<Array> result = NanNew<Array>(corrections.size());
-  for (size_t i = 0; i < corrections.size(); ++i) {
-    const std::string& word = corrections[i];
-    result->Set(i, NanNew<String>(word.data(), word.size()));
+    NanReturnValue(args.This());
   }
 
-  NanReturnValue(result);
-}
+  static NAN_METHOD(SetDictionaryDirectory) {
+    NanScope();
 
-void Init(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "init", PlatformInit);
-  NODE_SET_METHOD(exports, "isMisspelled", IsMisspelled);
-  NODE_SET_METHOD(exports,
-                  "getCorrectionsForMisspelling",
-                  GetCorrectionsForMisspelling);
+    if (args.Length() < 1) {
+      return NanThrowError("Bad argument");
+    }
+
+    Spellchecker* that = ObjectWrap::Unwrap<Spellchecker>(args.Holder());
+    std::string directory = *String::Utf8Value(args[0]);
+    that->impl->SetDictionaryDirectory(directory);
+    NanReturnUndefined();
+  }
+
+  static NAN_METHOD(IsMisspelled) {
+    NanScope();
+    if (args.Length() < 1) {
+      return NanThrowError("Bad argument");
+    }
+
+    Spellchecker* that = ObjectWrap::Unwrap<Spellchecker>(args.Holder());
+    std::string word = *String::Utf8Value(args[0]);
+
+    NanReturnValue(NanNew<Boolean>(that->impl->IsMisspelled(word)));
+  }
+
+  static NAN_METHOD(GetCorrectionsForMisspelling) {
+    NanScope();
+    if (args.Length() < 1) {
+      return NanThrowError("Bad argument");
+    }
+
+    Spellchecker* that = ObjectWrap::Unwrap<Spellchecker>(args.Holder());
+
+    std::string word = *String::Utf8Value(args[0]);
+    std::vector<std::string> corrections =
+      that->impl->GetCorrectionsForMisspelling(word);
+
+    Local<Array> result = NanNew<Array>(corrections.size());
+    for (size_t i = 0; i < corrections.size(); ++i) {
+      const std::string& word = corrections[i];
+      result->Set(i, NanNew<String>(word.data(), word.size()));
+    }
+
+    NanReturnValue(result);
+  }
+
+  Spellchecker() {
+    // TODO: Set impl
+    impl = new HunspellSpellchecker();
+  }
+
+  // actual destructor
+  virtual ~Spellchecker() {
+    delete impl;
+  }
+
+ public:
+  static void Init(Handle<Object> exports) {
+    Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(Spellchecker::New);
+
+    tpl->SetClassName(NanSymbol("Spellchecker"));
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    NODE_SET_METHOD(tpl->InstanceTemplate(), "setDictionaryDirectory", Spellchecker::SetDictionaryDirectory);
+    NODE_SET_METHOD(tpl->InstanceTemplate(), "getCorrectionsForMisspelling", Spellchecker::GetCorrectionsForMisspelling);
+    NODE_SET_METHOD(tpl->InstanceTemplate(), "isMisspelled", Spellchecker::IsMisspelled);
+
+    exports->Set(NanNew("Spellchecker"), tpl->GetFunction());
+  }
+};
+
+void Init(Handle<Object> exports, Handle<Object> module) {
+  Spellchecker::Init(exports);
 }
 
 }  // namespace
