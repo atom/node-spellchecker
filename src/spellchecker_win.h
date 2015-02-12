ebfffc47e69d891dbf0c0fd0e477640e6f095619
Paul Betts <paul@paulbetts.org>
2015-02-11 17:07:20 -0800

    Implement Windows checker using Win8 Spellcheck API

--- src/spellchecker_win.h
+++ src/spellchecker_win.h
@@ -16,6 +16,10 @@ public:
   bool SetDictionary(const std::string& language, const std::string& path);
   std::vector<std::string> GetCorrectionsForMisspelling(const std::string& word);
   bool IsMisspelled(const std::string& word);
+
+private:
+  ISpellChecker* currentSpellchecker;
+  ISpellCheckerFactory* spellcheckerFactory;
 };
 
 }  // namespace spellchecker
