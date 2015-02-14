b6e424d31b1a006e312dd2f15e253e542a43ffca
Paul Betts <paul@paulbetts.org>
2015-02-13 16:07:53 -0800

    Update the Mac spellchecker to have similar features

--- src/spellchecker_mac.h
+++ src/spellchecker_mac.h
@@ -3,13 +3,23 @@
 
 #include "spellchecker.h"
 
+#import <Cocoa/Cocoa.h>
+#import <dispatch/dispatch.h>
+
 namespace spellchecker {
 
 class MacSpellchecker : public SpellcheckerImplementation {
 public:
+  MacSpellchecker();
+  ~MacSpellchecker();
+
   bool SetDictionary(const std::string& language, const std::string& path);
+  std::vector<std::string> GetAvailableDictionaries(const std::string& path);
   std::vector<std::string> GetCorrectionsForMisspelling(const std::string& word);
   bool IsMisspelled(const std::string& word);
+
+private:
+  NSSpellChecker* spellChecker;
 };
 
 }  // namespace spellchecker
